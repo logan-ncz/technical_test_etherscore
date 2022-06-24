5b581693756351fe435aaaa215356188729c0662
Logan Nochez <logannochez@hotmail.fr>
2022-06-24 02:45:44 +0200

    uninstall redux (finally useless)

--- src/index.js
+++ src/index.js
@@ -5,19 +5,13 @@ import './sass/main.scss';
 
 import './services/i18n';
 
-import { Provider } from 'react-redux/es/exports';
-
-import store from './redux/store';
-
 import ThemeContextWrapper from './theme/ThemeWrapper';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeContextWrapper>
-        <App />
-      </ThemeContextWrapper>
-    </Provider>
+    <ThemeContextWrapper>
+      <App />
+    </ThemeContextWrapper>
   </React.StrictMode>
 );
